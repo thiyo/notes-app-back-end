f2250f50fadf2ff42fcdaab1d52c55dad89b039d
Muhammad Trio Maulana Putra <triomp58@gmail.com>
2023-08-14 22:24:00 +0700

    authorization error

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -14,29 +14,32 @@ class NotesHandler{
     
     async postNoteHandler(request, res) {
         try {
-            this._validator.validateNotePayload(request.payload);
-            const { title = 'untitled', body, tags } = request.payload; //dapatkan nilai title, tags, dan body dari objek request.payload
-            const noteId = await this._service.addNote({title, body, tags}); //method dari NotesService.js
-
-            const response = res.response({
-                status: 'success',
-                message: 'Catatan berhasil ditambahkan',
-                data: {
-                    noteId,
-                },
-            });
-            response.code(201);
-            return response;
-            
+          this._validator.validateNotePayload(request.payload);
+          const { title = 'untitled', body, tags } = request.payload;
+          const { id: credentialId } = request.auth.credentials;
+          
+          const noteId = await this._service.addNote({
+            title, body, tags, owner: credentialId,
+          });
+       
+          const response = res.response({
+            status: 'success',
+            message: 'Catatan berhasil ditambahkan',
+            data: {
+              noteId,
+            },
+          });
+          response.code(201);
+          return response;
         } catch (error) {
-            if (error instanceof ClientError){
-                const response = res.response({
-                    status: 'fail',
-                    message: error.message,
-                });
-                response.code(error.statusCode);
-                return response;
-            }
+          if (error instanceof ClientError) {
+            const response = res.response({
+              status: 'fail',
+              message: error.message,
+            });
+            response.code(error.statusCode);
+            return response;
+          }
 
             //server error!!
             const response = res.response({
@@ -50,8 +53,9 @@ class NotesHandler{
         
     }
 
-    async getNotesHandler() {
-        const notes = await this._service.getNotes();
+    async getNotesHandler(request) {
+        const { id: credentialId } = request.auth.credentials;
+        const notes = await this._service.getNotes(credentialId);
         return{
             status: 'success',
             data: {
@@ -63,7 +67,11 @@ class NotesHandler{
     async getNoteByIdHandler(request, res){
         try {
             const {id} = request.params;
+            const { id: credentialId } = request.auth.credentials;
+            
+            await this._service.verifyNoteOwner(id, credentialId);            
             const note = await this._service.getNoteById(id);
+
             return{
                 status: 'success',
                 data: {
@@ -95,8 +103,11 @@ class NotesHandler{
             this._validator.validateNotePayload(request.payload);
             const { title, body, tags } = request.payload;
             const {id} = request.params;
-
+            const { id: credentialId } = request.auth.credentials;
+            
+            await this._service.verifyNoteOwner(id, credentialId);            
             await this._service.editNoteById(id, { title, body, tags });
+
             return{
                 status: 'success',
                 message: 'Catatan berhasil diperbarui',
@@ -124,7 +135,11 @@ class NotesHandler{
     async deleteNoteByIdHandler(request, res){
         try {
             const {id} = request.params;
+            const { id: credentialId } = request.auth.credentials;
+            
+            await this._service.verifyNoteOwner(id, credentialId);
             await this._service.deleteNoteById(id);
+            
             return{
                 status: 'success',
                 message: 'Catatan berhasil dihapus',
